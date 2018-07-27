b3b32a09e2da654ad8ad7ef6f0b3427a49912334
KartikaAishah <41768879+KartikaAishah@users.noreply.github.com>
2018-07-27 11:44:26 +0800

    Add files via upload

---
+++ ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  AIyll
+//
+//  Created by mo on 2018/7/27.
+//  Copyright © 2018年 Fdcd. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
